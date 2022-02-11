f007a7d5ba9ce9d57c3e5a07e112a57b74aa28c8
Ganesh Patil <ganeshbuddy.patil@gmail.com>
2022-02-11 22:54:33 +0530

    Added basic UI

--- DigitalAnalogClock/ContentView.swift
+++ DigitalAnalogClock/ContentView.swift
@@ -9,8 +9,46 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        DigitView()
+    }
+}
+
+enum ClockState: String {
+    case Left
+    case Right
+    case Top
+    case Bottom
+    case TopRight
+    case RightBottom
+    case BottomLeft
+    case LeftTop
+    case TopBottom
+    case LeftRight
+    case Neutral
+}
+
+struct DigitView: View {
+    var body: some View {
+        VStack {
+            ForEach(0..<6) { _ in
+                HStack() {
+                    ForEach(0..<5) { _ in
+                        ClockView()
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ClockView: View {
+    @State var state: ClockState = .Neutral
+    var body: some View {
+        ZStack {
+            Circle().stroke(lineWidth: 3)
+            Text(state.rawValue)
+                .font(.caption2)
+        }
     }
 }
 
