d0b891a65b648b40ef674866ffe0688dd338d6c7
GaneshPatil4457 <gp4457@zebra.com>
2022-02-11 22:06:18 +0530

    Initial Commit

---
+++ DigitalAnalogClock/DigitalAnalogClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigitalAnalogClockApp.swift
+//  DigitalAnalogClock
+//
+//  Created by Patil, Ganesh on 11/02/22.
+//
+
+import SwiftUI
+
+@main
+struct DigitalAnalogClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
